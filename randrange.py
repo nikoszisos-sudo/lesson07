5720ebf0c2c3fdcda1d57120280aef2971d728bc
Nikolaos Zisos <nikoszisos@gmail.com>
2026-04-26 18:37:55 +0300

    nnn

--- randrange.py
+++ randrange.py
@@ -8,21 +8,21 @@ for x in range(10): # prints 10 times everything under this line
 
     my_column = set()
 
-    while len(my_column) < 2: # 2 random numbers from 10 to 19
+    while len(my_column) < 2: # 2 random numbers from 10 to 19 adding 2 of 6 numbers in set
         my_column.add(randrange(10,20))
     #print(my_column)
 
-    while len(my_column) < 4: # 2 random numbers from 20 to 39
+    while len(my_column) < 4: # 2 random numbers from 20 to 39 adding 4 of 6 numbers in set
         my_column.add(randrange(20,40))
     #print(my_column)
 
-    while len(my_column) < 5: # 1 random numbers from 1 to 9 even number
+    while len(my_column) < 5: # 1 random numbers from 1 to 9 even number adding 5 of 6 numbers in set
       my_num = randrange(1,10)
       if my_num % 2 == 0:
          my_column.add(my_num)
     #print(my_column)
 
-    while len(my_column) < 6: # 1 random numbers from 40 to 49 odd number
+    while len(my_column) < 6: # 1 random numbers from 40 to 49 odd number adding 6 of 6 numbers in set
         my_num = randrange(40, 50)
         if my_num % 2 != 0:
          my_column.add(my_num)
