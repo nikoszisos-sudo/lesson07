81f5c8f52eb6cea07ab6b0dc4a920d793db0d62c
Nikolaos Zisos <nikoszisos@gmail.com>
2026-04-26 10:19:48 +0300

    nnn

--- randrange.py
+++ randrange.py
@@ -6,36 +6,22 @@ for x in range(10): # prints 10 times everything under this line
 
     my_column = set()
 
-    my_randrange = randrange(10,20) # 2 random numbers from 10 to 19
-    my_column.add(my_randrange)
-    for i in range(1,2):
-        while True:
-            my_randrange = randrange(10,20)
-            if my_randrange not in my_column:
-                my_column.add(my_randrange)
-                break
+    while len(my_column) < 2: # 2 random numbers from 10 to 19
+        my_column.add(randrange(10,20))
     #print(my_column)
 
-    my_randrange = randrange(20,40) # 2 random numbers from 20 to 39
-    my_column.add(my_randrange)
-    for i in range(1,2):
-        while True:
-            my_randrange = randrange(20,40)
-            if my_randrange not in my_column:
-                my_column.add(my_randrange)
-                break
+    while len(my_column) < 4: # 2 random numbers from 20 to 39
+        my_column.add(randrange(20,40))
     #print(my_column)
 
-    while True:
-        my_randrange = randrange(1, 10)  # 1 random numbers from 1 to 9 even number
-        if my_randrange % 2 == 0:
-         my_column.add(my_randrange)
-         break
+    while len(my_column) < 5: # 1 random numbers from 1 to 9 even number
+      my_num = randrange(1,10)
+      if my_num % 2 == 0:
+         my_column.add(my_num)
     #print(my_column)
 
-    while True:
-        my_randrange = randrange(40, 50)  # 1 random numbers from 40 to 49 odd number
-        if my_randrange % 2 != 0:
-         my_column.add(my_randrange)
-         break
+    while len(my_column) < 6: # 1 random numbers from 40 to 49 odd number
+        my_num = randrange(40, 50)
+        if my_num % 2 != 0:
+         my_column.add(my_num)
     print(my_column)
