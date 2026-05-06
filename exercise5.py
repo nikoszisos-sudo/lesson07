f026346f3c492a73219b63275f5d5cba11848f6c
Nikolaos Zisos <nikoszisos@gmail.com>
2026-05-06 20:23:53 +0300

    nnn

--- exercise5.py
+++ exercise5.py
@@ -18,18 +18,59 @@ for i in range(5):
 print(player1)
 print(player2)
 
+#-------------------kare---------------------------------------
 
 cnt = 0
 for card in player1:
     if card[1]=="Ace":
         cnt += 1
-print("Player1 has " + str(cnt) + " Aces")
+if cnt == 4:
+    print("Player1 has " + str(cnt) + " Aces. He's got quads!")
+else:
+    print("Player1 has " + str(cnt) + " Aces.")
+
+
 
 cnt = 0
 for card in player2:
     if card[1]=="Ace":
         cnt += 1
-print("Player2 has " + str(cnt) + " Aces")
+if cnt == 4:
+    print("Player2 has " + str(cnt) + " Aces. He's got quads!")
+else:
+    print("Player2 has " + str(cnt) + " Aces.")
+
+
+#---------------------------kenta---------------------------------
+
+player_1_kenta = []
+player_2_kenta = []
+
+for card in player1:
+    if card[1]=="Ace":
+        player_1_kenta.append(1)
+    elif card[1]=="Jack":
+        player_1_kenta.append(11)
+    elif card[1]=="Queen":
+        player_1_kenta.append(12)
+    elif card[1]=="King":
+        player_1_kenta.append(13)
+    else:
+        player_1_kenta.append(card[1])
+print (sorted(player_1_kenta))
+
+for card in player2:
+    if card[1]=="Ace":
+        player_2_kenta.append(1)
+    elif card[1]=="Jack":
+        player_2_kenta.append(11)
+    elif card[1]=="Queen":
+        player_2_kenta.append(12)
+    elif card[1]=="King":
+        player_2_kenta.append(13)
+    else:
+        player_2_kenta.append(card[1])
+print (sorted(player_2_kenta))
 
 
 
