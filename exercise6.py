df3e131c167296076b361bb4b0a24d0656647543
Nikolaos Zisos <nikoszisos@gmail.com>
2026-05-10 22:04:24 +0300

    nnn

---
+++ exercise6.py
@@ -0,0 +1,26 @@
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
+print ("+---+---+---+") #prints the empty board starting from line 2 then line 1 and then line 0
+print ("| " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " |")
+print ("+---+---+---+")
+print ("| " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " |")
+print ("+---+---+---+")
+print ("| " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |")
+print ("+---+---+---+")
+
+player = "O"
+
+for i in range(9): #it plays X and O starting from X
+    if player == "O":
+        player = "X"
+    else:
+        player = "O"
+else:
+    print("+---+---+---+")
+    print("| " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " |")
+    print("+---+---+---+")
+    print("| " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " |")
+    print("+---+---+---+")
+    print("| " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |")
+    print("+---+---+---+")
+    print("draw")
