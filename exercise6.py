1a2f7b054f84bee6056d807eb6158e4e8b78e8b1
Nikolaos Zisos <nikoszisos@gmail.com>
2026-05-13 20:29:33 +0300

    nnn

--- exercise6.py
+++ exercise6.py
@@ -1,7 +1,5 @@
 board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-
-
 player = "O"
 
 for i in range(9):
@@ -19,19 +17,36 @@ for i in range(9):
     else:
         player = "O"
 
-    print ("Player " + player + " plays")
-
     while True:
+        print("Player " + player + " plays")
         row = int(input("Enter row number: "))
         col = int(input("Enter column number: "))
         if row < 0 or row > 2 or col < 0 or col > 2:
-            print("Invalid row or column number")
+            print("Invalid row or column number (0-2)")
             continue
         elif board[row][col] != " ":
             print("Row or column occupied")
             continue
         else:
+            board[row][col] = player
             break
+    if (board[0][0] == board[0][1] == board[0][2]) and board[0][0] != " ":
+        winner = player
+    elif (board[1][0] == board[1][1] == board[1][2]) and board[1][0] != " ":
+        winner = player
+    elif (board[2][0] == board[2][1] == board[2][2]) and board[2][0] != " ":
+        winner = player
+    elif (board[0][0] == board[1][0] == board[2][0]) and board[0][0] != " ":
+        winner = player
+    elif (board[0][1] == board[1][1] == board[2][1]) and board[0][1] != " ":
+        winner = player
+    elif (board[0][2] == board[1][2] == board[2][2]) and board[0][2] != " ":
+        winner = player
+    elif (board[0][0] == board[1][1] == board[2][2]) and board[0][0] != " ":
+        winner = player
+    else:
+        winner = player
+    print("Player " + winner + " wins!")
 
 else:
     print("+---+---+---+")
